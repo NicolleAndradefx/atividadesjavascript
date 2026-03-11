dca8ff2851577adc61b9889992a3dd9a8c108977
NICOLLE AILEEN ARAUJO DE ANDRADE <nicolle.aaandrade@senacsp.edu.br>
2026-03-11 09:29:41 -0300

    exercicios

--- 05-atividade/script.js
+++ 05-atividade/script.js
@@ -76,3 +76,97 @@ for(let b = 0; b < matriz.length; b++){
         console.log("[" + b + "][" + c + "] = " + matriz[b][c] )
     }
 }
+
+
+// 1️ Cadastro de cliente – Empresa de bolo de pote
+ 
+let clienteA = {
+    nome: "Maria",
+    telefone: "11999999999",
+    pedidoFavorito: "Bolo de pote de chocolate"
+};
+ 
+console.log(clienteA.nome);
+ 
+clienteA.cidade = "São Paulo";
+ 
+delete clienteA.telefone;
+ 
+console.log(clienteA);
+ 
+ 
+ 
+// 2️ Cadastro de produto – Confeitaria
+ 
+let bolo = {
+    sabor: "Morango",
+    preco: 15
+};
+ 
+let ingredientes = {
+    massa: "Baunilha",
+    recheio: "Creme de morango"
+};
+ 
+let produtoFinal = {};
+ 
+Object.assign(produtoFinal, bolo, ingredientes);
+console.log(produtoFinal);
+ 
+ 
+ 
+// 3️ Serviço do ateliê de costura
+ 
+let servico = {
+    tipo: "Ajuste de vestido",
+    preco: 50
+};
+ 
+servico.preco = 70;
+ 
+servico.p
+console.log(servico);
+ 
+ 
+ 
+// 4️ Ficha de paciente – Psicólogo
+ 
+let paciente = {
+    nome: "João",
+    idade: 30,
+    problema: "Ansiedade"
+};
+ 
+let propriedades = Object.keys(paciente);
+ 
+console.log(propriedades);
+ 
+ 
+ 
+// 5️ Cadastro de costureira – Ateliê de costura
+ 
+let costureira = {
+    nome: "Ana",
+    experiencia: 5,
+    especialidade: "Vestidos"
+};
+ 
+costureira.experiencia += 1;
+ 
+console.log(costureira);
+ 
+ 
+ 
+// 6️ Informações do terapeuta – Clínica de psicologia
+ 
+let terapeuta = {
+    nome: "Carlos",
+    especialidade: "Terapia Cognitivo-Comportamental",
+    cidade: "São Paulo"
+};
+ 
+let [nome, especialidade, cidade] = Object.values(terapeuta);
+ 
+console.log(nome);
+console.log(especialidade);
+console.log(cidade);
